f43d65c1743050c2679006d9da8f9a0f0adbc6d1
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-02 15:20:30 -0600

    Add visit, array  pop visit array

--- friends.rb
+++ friends.rb
@@ -1,4 +1,8 @@
 friends = ["Heather", "Anna", "Matt", "Mark", "Abby"]
-puts friends[3]
-puts friends[4]
-puts friends[0]
+# puts friends[3]
+# puts friends[4]
+# puts friends[0]
+
+places_visit = ["Washington", "New York", "Brazil", "France", "Costa Rica"]
+
+puts places_visit.pop
